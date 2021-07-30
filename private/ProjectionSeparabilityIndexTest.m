f9a9d375f87e200b56349673bfbf2245a7a688af
Aldo Acevedo <aldo.acevedo.toledo@gmail.com>
2021-07-30 21:50:12 +0200

    Add new test cases

--- private/ProjectionSeparabilityIndexTest.m
+++ private/ProjectionSeparabilityIndexTest.m
@@ -24,3 +24,26 @@ function TestPerfectSeparation(testCase)
     verifyEqual(testCase, actual.dataClustered, expected.dataClustered);
     verifyEqual(testCase, actual.sortedLabels, expected.sortedLabels);
 end
+
+function TestMixedSeparation(testCase)
+	input.matrix = [1 2; 3 4; 5 6; 7 8; 10 11; 12 13; 14 15; 16 17];
+    input.samples = {'sample2','sample1','sample1','sample1','sample2','sample2','sample2','sample1'}';
+    input.positive = {'sample1'};
+    input.formula = 'median';
+    
+    expected.psiP = 0.8857;
+    expected.psiROC = 0.5625;
+    expected.psiPR = 0.5015;
+    expected.psiMCC = 0.5000;
+    expected.dataClustered = {[3 4; 5 6; 7 8; 16 17], [1 2; 10 11; 12 13; 14 15]};
+    expected.sortedLabels = {'sample1','sample1','sample1','sample1','sample2','sample2','sample2','sample2'}';
+    
+    [actual.psiP, actual.psiROC, actual.psiPR, actual.psiMCC, actual.dataClustered, actual.sortedLabels] = ProjectionSeparabilityIndex(input.matrix, input.samples, input.positive, input.formula);
+    
+    verifyEqual(testCase, round(actual.psiP, 4), expected.psiP);
+    verifyEqual(testCase, round(actual.psiROC, 4), expected.psiROC);
+    verifyEqual(testCase, round(actual.psiPR, 4), expected.psiPR);
+    verifyEqual(testCase, round(actual.psiMCC, 4), expected.psiMCC);
+    verifyEqual(testCase, actual.dataClustered, expected.dataClustered);
+    verifyEqual(testCase, actual.sortedLabels, expected.sortedLabels);
+end
--- wrappers/python/test_projection_separability_index.py
+++ wrappers/python/test_projection_separability_index.py
@@ -27,3 +27,23 @@ class TestProjectionSeparabilityIndex(unittest.TestCase):
         self.assertEqual(expected_psi_roc, round(actual_psi_roc, 4))
         self.assertEqual(expected_psi_pr, round(actual_psi_pr, 4))
         self.assertEqual(expected_psi_mcc, round(actual_psi_mcc, 4))
+
+    def test_mixed_separation(self):
+        input_matrix = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [10, 11], [12, 13], [14, 15], [16, 17]])
+        input_labels = np.array(
+            ['sample2', 'sample1', 'sample1', 'sample1', 'sample2', 'sample2', 'sample2', 'sample1'])
+        input_positive = np.array(['sample1'])
+        input_formula = 'median'
+
+        expected_psi_p = 0.8857
+        expected_psi_roc = 0.5625
+        expected_psi_pr = 0.5015
+        expected_psi_mcc = 0.5000
+
+        psi = ProjectionSeparabilityIndex(input_matrix, input_labels, input_positive, input_formula)
+        actual_psi_p, actual_psi_roc, actual_psi_pr, actual_psi_mcc = psi.calculate()
+
+        self.assertEqual(expected_psi_p, round(actual_psi_p, 4))
+        self.assertEqual(expected_psi_roc, round(actual_psi_roc, 4))
+        self.assertEqual(expected_psi_pr, round(actual_psi_pr, 4))
+        self.assertEqual(expected_psi_mcc, round(actual_psi_mcc, 4))
